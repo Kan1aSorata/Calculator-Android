d746aac64a3864806245c765ced999f0b2ca33f2
Fengyu He <hefengyu_sx@qiyi.com>
2021-09-16 17:24:52 +0800

    feat: 添加按钮点击事件绑定, 添加基本计算逻辑, 添加括号匹配等错误输入处理

--- app/src/main/java/com/fengyuhe/calculator/MainActivity.kt
+++ app/src/main/java/com/fengyuhe/calculator/MainActivity.kt
@@ -1,38 +1,130 @@
 package com.fengyuhe.calculator
 
+import android.annotation.SuppressLint
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
+import com.fengyuhe.calculator.databinding.ActivityMainBinding
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
+    private var mBinding: ActivityMainBinding? = null
+    private var exp = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding!!.root)
 
-        //View：表达式
-        val textView_expression = findViewById<TextView>(R.id.expression) //View：表达式
-        textView_expression.setOnClickListener {
-            
+        mBinding!!.btnPlus.setOnClickListener { addExp("+") }
+        mBinding!!.btnMinus.setOnClickListener { addExp("-") }
+        mBinding!!.btnMultipy.setOnClickListener { addExp("*") }
+        mBinding!!.btnDivide.setOnClickListener { addExp("/") }
+        mBinding!!.btnBracketLeft.setOnClickListener { addExp("(") }
+        mBinding!!.btnBracketRight.setOnClickListener { addExp(")") }
+        mBinding!!.btnDot.setOnClickListener { addExp(".") }
+        mBinding!!.btnNegative.setOnClickListener { addExp("-") }
+
+        mBinding!!.btnAC.setOnClickListener {
+            mBinding!!.expression.text = ""
+            mBinding!!.result.text = ""
+            exp = ""
         }
 
-        val textView_result = findViewById<TextView>(R.id.result) //View：显示结果
-        val btn_ac = findViewById<Button>(R.id.btn_AC) //按钮：删除所有
-        val btn_delete = findViewById<Button>(R.id.btn_delete) //按钮：删除一位
-        val btn_plus = findViewById<Button>(R.id.btn_plus) //按钮：+
-        val btn_minus = findViewById<Button>(R.id.btn_minus) //按钮：-
-        val btn_divide = findViewById<Button>(R.id.btn_divide) //按钮：/
-        val btn_multipy = findViewById<Button>(R.id.btn_multipy) //按钮：x
-        val btn_equal = findViewById<Button>(R.id.btn_equal) //按钮：等于
-        val btn_bracket_left = findViewById<Button>(R.id.btn_bracket_left) //按钮：(
-        val btn_bracket_right = findViewById<Button>(R.id.btn_bracket_right) //按钮：)
-        val btn_negative = findViewById<Button>(R.id.btn_negative) //按钮：+/-
+        mBinding!!.btnDelete.setOnClickListener {
+            exp = exp.dropLast(1)
+            mBinding!!.result.text = exp
+        }
 
+        mBinding!!.btnEqual.setOnClickListener {
+            val res = eval(exp)
+            addExp("=")
+            mBinding!!.expression.text = exp
+            mBinding!!.result.text = res.toString()
+            exp = ""
+        }
 
+        val btnDigs = arrayOf(mBinding!!.btn0, mBinding!!.btn1, mBinding!!.btn2, mBinding!!.btn3, mBinding!!.btn4, mBinding!!.btn5, mBinding!!.btn6, mBinding!!.btn7, mBinding!!.btn8, mBinding!!.btn9)
+        btnDigs.forEach { button ->
+            button.setOnClickListener {
+                addExp(
+                    when(it) {
+                        mBinding!!.btn0 -> "0"
+                        mBinding!!.btn1 -> "1"
+                        mBinding!!.btn2 -> "2"
+                        mBinding!!.btn3 -> "3"
+                        mBinding!!.btn4 -> "4"
+                        mBinding!!.btn5 -> "5"
+                        mBinding!!.btn6 -> "6"
+                        mBinding!!.btn7 -> "7"
+                        mBinding!!.btn8 -> "8"
+                        mBinding!!.btn9 -> "9"
+                        else -> ""
+                    }
+                )
+            }
+        }
+    }
 
+    private fun addExp(element: String) {
+        exp = exp.plus(element)
+        mBinding!!.result.text = exp
+    }
 
+    @SuppressLint("SetTextI18n")
+    private fun eval(expr: String): Float {
+        var index = 0 // current index
+        val skipWhile = { cond: (Char) -> Boolean -> while (index < expr.length && cond(expr[index])) index++ }
+        val tryRead = { c: Char -> (index < expr.length && expr[index] == c).also { if (it) index++ } }
+        val skipWhitespaces = { skipWhile { it.isWhitespace() } }
+        val tryReadOp = { op: Char -> skipWhitespaces().run { tryRead(op) }.also { if (it) skipWhitespaces() } }
+        var rootOp: () -> Float = { 0.0f }
 
+        val num = {
+            if (tryReadOp('(')) {
+                rootOp().also {
+                    tryReadOp(')').also {
+                        if (!it){
+                            mBinding!!.result.text = "Missing at: $index"
+                            exp = ""
+                        }
+                    }
+                }
+            } else {
+                val start = index
+                tryRead('-') or tryRead('+')
+                skipWhile { it.isDigit() || it == '.' }
+                try {
+                    expr.substring(start, index).toFloat()
+                } catch (e: NumberFormatException) {
+                    mBinding!!.result.text = "Invalid number at:${start}"
+                    exp = ""
+                    "1.0".toFloat()
+                }
+            }
+        }
+
+        fun binary(left: () -> Float, op: Char): List<Float> = mutableListOf(left()).apply {
+            while (tryReadOp(op)) addAll(binary(left, op))
+        }
+
+        val div = { binary(num, '/').reduce { a, b -> a / b } }
+        val mul = { binary(div, '*').reduce { a, b -> a * b } }
+        val sub = { binary(mul, '-').reduce { a, b -> a - b } }
+        val add = { binary(sub, '+').reduce { a, b -> a + b } }
+
+        rootOp = add
+        return rootOp().also {
+            if (index < expr.length) {
+                mBinding!!.result.text = "Invalid expression at:${index}"
+                exp = ""
+            }
+        }
     }
 }
